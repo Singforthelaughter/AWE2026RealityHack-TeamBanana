ccda5ef090dd505e3a49f6eefd709d58aed43bf3
GOWAAA <boon@gowaaa.com>
2026-06-16 05:11:49 +0800

    update instruction

--- TeamBanana/Assets/_Boon/Instruction/Scripts/InstructionManager.ts
+++ TeamBanana/Assets/_Boon/Instruction/Scripts/InstructionManager.ts
@@ -6,14 +6,20 @@ import { CustomHandHintAnimation } from "./CustomHandHintAnimation"
 export class InstructionManager extends BaseScriptComponent {
   @input text!: Text
   @input handHint!: CustomHandHintAnimation
+  @input onboardingVisual!: SceneObject
 
   private selfTransform!: Transform
 
   onAwake() {
     this.selfTransform = this.sceneObject.getTransform()
     this.selfTransform.setLocalScale(vec3.zero())
+    this.onboardingVisual.getTransform().setLocalScale(vec3.zero())
 
     this.createEvent("OnStartEvent").bind(() => {
+      LSTween.scaleToLocal(this.onboardingVisual.getTransform(), new vec3(0.5, 0.5, 0.5), 1000)
+        .easing(Easing.Sinusoidal.Out)
+        .start()
+
       LSTween.scaleToLocal(this.selfTransform, vec3.one(), 1000).easing(Easing.Sinusoidal.Out).start()
       timeManager.setTimeout(() => {
         this.handHint.play()
